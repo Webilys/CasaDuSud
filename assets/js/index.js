bc4f7dacf590e22d6867792fbe357955ad427c72
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-05-23 14:32:05 +0200

    23/05/24 14:32

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+//------------MENU RESPONSIVE--------------
+
 const menuIcons = document.getElementById("menuIcons");
 const openIcon = document.getElementById("openIcon");
 const closeIcon = document.getElementById("closeIcon");
@@ -19,10 +21,47 @@ function openNav() {
   nav.style.display = "block";
 }
 
-//fonction ouvre le menu
+//fonction ferme le menu
 
 function closeNav() {
   closeIcon.style.display = "none";
   openIcon.style.display = "block";
   nav.style.display = "none";
+}
+
+//-------------PAGE LE LOGEMENT-----------------
+
+//Slide images
+const imgGalery = document.getElementById("imgGalery");
+
+const img1 = "url(./assets/img/1.webp) center/cover";
+const img2 = "url(./assets/img/2.webp) center/cover";
+const img3 = "url(./assets/img/3.webp) center/cover";
+const img4 = "url(./assets/img/4.webp) center/cover";
+const img5 = "url(./assets/img/5.webp) center/cover";
+const img6 = "url(./assets/img/6.webp) center/cover";
+imgGalery.style.background = img1;
+
+// imgBg = imgGalery.style.background;
+
+function precedent() {
+  if ((imgGalery.style.background = img1)) {
+    imgGalery.style.background = img6;
+  } else {
+    if ((imgGalery.style.background = img2)) {
+      imgGalery.style.background = img1;
+    }
+    if ((imgGalery.style.background = img3)) {
+      imgGalery.style.background = img2;
+    }
+    if ((imgGalery.style.background = img4)) {
+      imgGalery.style.background = img3;
+    }
+    if ((imgGalery.style.background = img5)) {
+      imgGalery.style.background = img4;
+    }
+    if ((imgGalery.style.background = img6)) {
+      imgGalery.style.background = img5;
+    }
+  }
 }
