bca95bc3ecd834bf81a813e2e9b4d325c1fddebd
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-05-23 06:48:22 +0200

    23/05/24 06:48

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const menuIcons = document.getElementById("menuIcons");
+const openIcon = document.getElementById("openIcon");
+const closeIcon = document.getElementById("closeIcon");
+const nav = document.querySelector("nav");
+
+//Le menu reste fermé au chargement de la page
+
+function windowLoad() {
+  if ((window.width = 1250)) {
+    nav.style.display = none;
+  }
+}
+
+//fonction ouvre le menu
+
+function openNav() {
+  openIcon.style.display = "none";
+  closeIcon.style.display = "block";
+  nav.style.display = "block";
+}
+
+//fonction ouvre le menu
+
+function closeNav() {
+  closeIcon.style.display = "none";
+  openIcon.style.display = "block";
+  nav.style.display = "none";
+}
