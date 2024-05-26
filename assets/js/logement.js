7a58a10c0600fd90f0ee43bbe100b92c4dc9c0fe
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-05-26 13:56:24 +0200

    26/05/24 13:56

--- assets/js/logement.js
+++ assets/js/logement.js
@@ -9,27 +9,22 @@ const infoLingeMaisonList = document.getElementById("infoLingeMaisonList");
 // Fonction pour ouvrir le slide en cliquant sur les images
 
 function slide1() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img1";
 }
-
 function slide2() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img2";
 }
-
 function slide3() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img3";
 }
-
 function slide4() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img4";
 }
-
 function slide5() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img5";
 }
-
 function slide6() {
-  window.location.href = "./slide.html";
+  window.location.href = "./slide.html?img=img6";
 }
 
 //Description longue
--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,5 +1,45 @@
 //-------------PAGE SLIDE-----------------
 
+function windowLoadSlide() {
+  const params = new URLSearchParams(window.location.search);
+  const img = params.get("img");
+
+  // Hide all images initially
+  const allDivImages = [
+    divImage1,
+    divImage2,
+    divImage3,
+    divImage4,
+    divImage5,
+    divImage6,
+  ];
+  allDivImages.forEach((div) => (div.style.display = "none"));
+
+  // Display the correct image based on the query parameter
+  switch (img) {
+    case "img1":
+      divImage1.style.display = "grid";
+      break;
+    case "img2":
+      divImage2.style.display = "grid";
+      break;
+    case "img3":
+      divImage3.style.display = "grid";
+      break;
+    case "img4":
+      divImage4.style.display = "grid";
+      break;
+    case "img5":
+      divImage5.style.display = "grid";
+      break;
+    case "img6":
+      divImage6.style.display = "grid";
+      break;
+    default:
+      divImage1.style.display = "grid"; // Default to image 1 if no query parameter is found
+  }
+}
+
 const iconCloseVisualisator = document.getElementById("iconCloseVisualisator");
 const slideGallery = document.getElementById("slide-gallery");
 const divImage1 = document.getElementById("image1");
@@ -14,6 +54,7 @@ const imgGalery3 = document.getElementById("imgGalery3");
 const imgGalery4 = document.getElementById("imgGalery4");
 const imgGalery5 = document.getElementById("imgGalery5");
 const imgGalery6 = document.getElementById("imgGalery6");
+
 const img1 = "url(./assets/img/1.webp) center/cover";
 const img2 = "url(./assets/img/2.webp) center/cover";
 const img3 = "url(./assets/img/3.webp) center/cover";
@@ -21,90 +62,91 @@ const img4 = "url(./assets/img/4.webp) center/cover";
 const img5 = "url(./assets/img/5.webp) center/cover";
 const img6 = "url(./assets/img/6.webp) center/cover";
 
-//fonction pour fermer le visualisateur
-
+// Fonction pour fermer le visualisateur
 function closeVisualisator() {
   window.history.back();
 }
 
-//Slide images
-
+// Initialise l'affichage des images
 divImage1.style.display = "grid";
 divImage2.style.display = "none";
 divImage3.style.display = "none";
 divImage4.style.display = "none";
 divImage5.style.display = "none";
 divImage6.style.display = "none";
+
+// Applique les arrière-plans aux galeries
 imgGalery1.style.background = img1;
+imgGalery2.style.background = img2;
+imgGalery3.style.background = img3;
+imgGalery4.style.background = img4;
+imgGalery5.style.background = img5;
+imgGalery6.style.background = img6;
+
+function precedent(index) {
+  const images = [
+    divImage1,
+    divImage2,
+    divImage3,
+    divImage4,
+    divImage5,
+    divImage6,
+  ];
+  images.forEach((div) => (div.style.display = "none"));
+  const prevIndex = (index - 1 + images.length) % images.length;
+  images[prevIndex].style.display = "grid";
+}
+
+function suivant(index) {
+  const images = [
+    divImage1,
+    divImage2,
+    divImage3,
+    divImage4,
+    divImage5,
+    divImage6,
+  ];
+  images.forEach((div) => (div.style.display = "none"));
+  const nextIndex = (index + 1) % images.length;
+  images[nextIndex].style.display = "grid";
+}
 
 function precedent1() {
-  imgGalery6.style.background = img6;
-  divImage1.style.display = "none";
-  divImage6.style.display = "grid";
+  precedent(1);
 }
-
 function precedent2() {
-  imgGalery1.style.background = img1;
-  divImage2.style.display = "none";
-  divImage1.style.display = "grid";
+  precedent(2);
 }
-
 function precedent3() {
-  imgGalery2.style.background = img2;
-  divImage3.style.display = "none";
-  divImage2.style.display = "grid";
+  precedent(3);
 }
-
 function precedent4() {
-  imgGalery3.style.background = img3;
-  divImage4.style.display = "none";
-  divImage3.style.display = "grid";
+  precedent(4);
 }
-
 function precedent5() {
-  imgGalery4.style.background = img4;
-  divImage5.style.display = "none";
-  divImage4.style.display = "grid";
+  precedent(5);
 }
-
 function precedent6() {
-  imgGalery5.style.background = img5;
-  divImage6.style.display = "none";
-  divImage5.style.display = "grid";
+  precedent(6);
 }
 
 function suivant1() {
-  imgGalery2.style.background = img2;
-  divImage1.style.display = "none";
-  divImage2.style.display = "grid";
+  suivant(1);
 }
-
 function suivant2() {
-  imgGalery3.style.background = img3;
-  divImage2.style.display = "none";
-  divImage3.style.display = "grid";
+  suivant(2);
 }
-
 function suivant3() {
-  imgGalery4.style.background = img4;
-  divImage3.style.display = "none";
-  divImage4.style.display = "grid";
+  suivant(3);
 }
-
 function suivant4() {
-  imgGalery5.style.background = img5;
-  divImage4.style.display = "none";
-  divImage5.style.display = "grid";
+  suivant(4);
 }
-
 function suivant5() {
-  imgGalery6.style.background = img6;
-  divImage5.style.display = "none";
-  divImage6.style.display = "grid";
+  suivant(5);
+}
+function suivant6() {
+  suivant(6);
 }
 
-function suivant6() {
-  imgGalery1.style.background = img1;
-  divImage6.style.display = "none";
-  divImage1.style.display = "grid";
-}
+window.onload = windowLoadSlide;
