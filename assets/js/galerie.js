602b61d85d74f35500dba5f501448eeb9b5df055
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-05-27 06:50:42 +0200

    27/05/24 06:50

---
+++ assets/js/galerie.js
@@ -0,0 +1,18 @@
+function slide1() {
+  window.location.href = "./slide.html?img=img1";
+}
+function slide2() {
+  window.location.href = "./slide.html?img=img2";
+}
+function slide3() {
+  window.location.href = "./slide.html?img=img3";
+}
+function slide4() {
+  window.location.href = "./slide.html?img=img4";
+}
+function slide5() {
+  window.location.href = "./slide.html?img=img5";
+}
+function slide6() {
+  window.location.href = "./slide.html?img=img6";
+}
